d5128a68d8b1491d45ec982591207a3fe173fc51
nrot <nrot13@gmail.com>
2021-08-26 15:29:10 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod model;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn model_macro(){
+        model!(User, );
+    }
+}
